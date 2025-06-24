8c13ef64d9022ce5b02677f0a01a5c75a3482c31
Anabelmo Feijó <anabelmofeijo@gmail.com>
2025-06-24 17:48:28 +0100

    Fourth Commit

--- app/routers/payment_router.py
+++ app/routers/payment_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.schemas.payment_schema import PaymentCreate
-from app.services.payment_service import PaymentService
+from app.schemas.payment_schema import PaymentCreate, PaymentMonthlyCreate
+from app.services.payment_service import PaymentService, MonthlyPaymentService
 
 router = APIRouter()
 
@@ -24,4 +24,37 @@ async def list_payments():
 async def list_payment_by_student(student_id: int):
     service = PaymentService()
     response = service.ListPaymentByStudent(student_id)
+    return response
+
+@router.delete("/delete/{id}")
+async def delete(id: int):
+    service = PaymentService()
+    response = service.DeletePayment(id)
+    return response
+
+
+# Monthly Payment
+
+@router.post("/monthly/create/")
+async def create_payment(data: PaymentMonthlyCreate):
+    service = MonthlyPaymentService
+    response = service.CreatePayment(new_data=data.dict())
+    return response
+
+@router.get("/monthly/list-payments/")
+async def list_payments():
+    service = MonthlyPaymentService()
+    response = service.ListPayment()
+    return response
+
+@router.get("/monthly/list-payment-by-student/{student_id}")
+async def list_payment_by_student(student_id: int):
+    service = MonthlyPaymentService()
+    response = service.ListPaymentByStudent(student_id)
+    return response
+
+@router.delete("/monthly/delete/{id}")
+async def delete(id: int):
+    service = PaymentService()
+    response = service.DeletePayment(id)
     return response
